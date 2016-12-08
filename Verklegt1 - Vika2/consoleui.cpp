0907036fe8c744e27c634cc9c49331bdf918f0de
Frank <streamerinn@gmail.com>
2016-12-08 21:13:12 +0000

    Lagadi getline ruglid, tok cout ur database, lykkja i addComputer

--- Verklegt1 - Vika2/consoleui.cpp
+++ Verklegt1 - Vika2/consoleui.cpp
@@ -60,14 +60,8 @@ void ConsoleUI::features()
     cout << endl;
 }
 
-
-
 void ConsoleUI::readComputers()
 {
-
-
-
-
     Computer temp;
 
     string tempComputerName = " ";
@@ -78,7 +72,6 @@ void ConsoleUI::readComputers()
 
     // Get name.
     cout << TAB << "Please enter a computer name: ";
-
     getline(cin, tempComputerName);
     while(tempComputerName.empty())
     {
@@ -156,6 +149,19 @@ void ConsoleUI::readComputers()
 
     temp.setBuilt(tempMade);
 
+    char cont;
+    cout << TAB << "Do you want to add another computer? Press Y/y for yes or N/n for no:  ";
+    cin >> cont;
+    if(cont == 'y' || cont == 'Y')
+    {
+        cin.ignore();
+        readComputers();
+    }
+    else
+    {
+        features();
+    }
+
     cService.create(temp);
 }
 
@@ -197,10 +203,6 @@ void ConsoleUI::displayScientistConnections(vector<Scientist> scientist)
     cout << TAB << "---------------------------" << endl;
 
 }
-
-
-
-
 
 // Leitar að tölvu í gagnagrunni.
 void ConsoleUI::searchComputer()
@@ -612,6 +614,7 @@ void ConsoleUI::readScientists()
     cin >> cont;
     if(cont == 'y' || cont == 'Y')
     {
+        cin.ignore();
         readScientists();
     }
     else
