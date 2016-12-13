8d1cace0c4b831a9cf648a1e2b9b00c312f69c4b
Bjartur <bjartur321@gmail.com>
2016-12-13 14:02:42 +0000

    Search virkar fyrir computers og scientists

---
+++ GUI-verklegtVika3/computertabledialog.h
@@ -0,0 +1,34 @@
+#ifndef COMPUTERTABLEDIALOG_H
+#define COMPUTERTABLEDIALOG_H
+
+#include <QDialog>
+#include <Qstring>
+#include <vector>
+#include <QDebug>
+
+#include "computer.h"
+#include "computerservice.h"
+
+namespace Ui {
+class ComputerTableDialog;
+}
+
+class ComputerTableDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit ComputerTableDialog(QWidget *parent = 0);
+    ~ComputerTableDialog();
+
+private slots:
+    void on_ComputerSearch_textChanged();
+
+private:
+    ComputerService computerService;
+    void displayAllComputers();
+    void displayComputers(vector<Computer> computers);
+    Ui::ComputerTableDialog *ui;
+};
+
+#endif // COMPUTERTABLEDIALOG_H
